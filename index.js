b6fd339963f6a62c87c4ed4a89accf1e860108e4
haevanhoe quihuata <haevanhoe@gmail.com>
2018-09-30 01:42:45 -0300

    a

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function generateMessages(){
 
 bot.on("message", message => {
     if(!message.channel.id === "495741200343760911") return;
-    if (message.author.id === "346409345069613056" && message.content.toLowerCase() == setupCMD){
+    if (message.author.id === "495816794678755328" && message.content.toLowerCase() == setupCMD){
         var toSend = generateMessages();
         let mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, reactions[idx]])];
         for (let mapObj of mappedArray){
